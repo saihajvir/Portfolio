628bdc9ff19ce37d2a0d2fccd7d2dfdc2eefc571
saihajvir <saihajvir@hotmail.com>
2022-01-09 16:09:53 -0800

    not much

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <div>
+      <Head>
+        <title>Saihaj Gill</title>
+        <link
+          rel='preload'
+          href='/fonts/Circular/CircularStd-Book.otf'
+          as='font'
+          crossOrigin=''
+        />
+      </Head>
+    </div>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
